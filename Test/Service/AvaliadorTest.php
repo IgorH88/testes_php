90092f8cfb9fcaa1d2ff892525c71f15eca9456b
Igor Azevedo <igorhenrique307@gmail.com>
2024-07-11 07:13:53 -0300

    feat: adiciona testes de menor valor

--- Test/Service/AvaliadorTest.php
+++ Test/Service/AvaliadorTest.php
@@ -28,6 +28,24 @@
             self::assertEquals(2500, $maiorValor);
         }
 
+        public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemCecrescente()
+        {
+            $leilao = new Leilao('Computador gamer');
+        
+            $usuMaria = new Usuario('Maria');
+            $usuJoao = new Usuario('João');
+        
+            $leilao->recebeLance(new Lance($usuMaria, 2000));
+            $leilao->recebeLance(new Lance($usuJoao, 2500));
+        
+            $leiloeiro = new Avaliador();
+        
+            $leiloeiro->avalia($leilao);
+            $menorValor = $leiloeiro->getMenorValor();
+        
+            self::assertEquals(2000, $menorValor);
+        }
+
         public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOremDecrescente()
         {
             $leilao = new Leilao('Computador gamer');
@@ -44,5 +62,23 @@
             $maiorValor = $leiloeiro->getMaiorValor();
         
             self::assertEquals(2500, $maiorValor);
+        }
+
+        public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOremDecrescente()
+        {
+            $leilao = new Leilao('Computador gamer');
+        
+            $usuMaria = new Usuario('Maria');
+            $usuJoao = new Usuario('João');
+        
+            $leilao->recebeLance(new Lance($usuJoao, 2500));
+            $leilao->recebeLance(new Lance($usuMaria, 2000));
+        
+            $leiloeiro = new Avaliador();
+        
+            $leiloeiro->avalia($leilao);
+            $menorValor = $leiloeiro->getMenorValor();
+        
+            self::assertEquals(2000, $menorValor);
         }
     }
