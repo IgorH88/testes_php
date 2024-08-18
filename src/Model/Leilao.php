bddfa67ba74450640918e9673f034de1a4cfbb08
Igor Azevedo <igorhenrique307@gmail.com>
2024-08-18 08:26:29 -0300

    feat: adicionando testes para mais que 5 lances e repetidos simultaneos

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -17,8 +17,47 @@ class Leilao
 
     public function recebeLance(Lance $lance)
     {
+        if (!empty($this->lances) && $this->verificaSeOLanceEDoMesmoUsuario($lance)) {
+            return;
+        }
+
+        $totalLancesUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+        if ($totalLancesUsuario >= 5) {
+            return;
+        }
+    
         $this->lances[] = $lance;
     }
+
+    /**
+     * @param Usuario $usuario
+     * @return Lance|mixed
+     */
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        $totalLancesUsuario = array_reduce(
+            $this->lances,
+            function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+                return $totalAcumulado;
+            },
+            0
+        );
+        return $totalLancesUsuario;
+    }
+
+    /**
+     * @param Lance $lance
+     * @return bool
+     */
+    private function verificaSeOLanceEDoMesmoUsuario(Lance $lance): bool
+    {
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $lance->getUsuario() == $ultimoLance->getUsuario();
+    }
+
 
     /**
      * @return Lance[]
